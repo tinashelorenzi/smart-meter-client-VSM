8a4db10dea0aaa5bba1a9ab0ae2364def8de8009
Tinashe Matanda <tinashelorenzi@protonmail.com>
2025-07-25 11:33:12 +0200

    Build fix productiom

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://poc-vsm.vertexcatalystgroup.co.za:562/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
